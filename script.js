274ff3ce8bffcc80d4dc6d7375b91e31db9bd7da
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-17 12:41:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let upper=text.toUpperCase();
+	let lower=text.toLowerCase();
+	let ans= lower+"-"+upper;
+	return ans;
 }
 
 // DO not change the code below
